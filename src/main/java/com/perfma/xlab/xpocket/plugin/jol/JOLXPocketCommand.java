f92c9d3fb1732ed5b3fe7daacbca47cb80eddd7e
LeonDou <yiwang_d@163.com>
2021-09-01 13:33:12 +0800

    Add some usages.

--- src/main/java/com/perfma/xlab/xpocket/plugin/jol/JOLXPocketCommand.java
+++ src/main/java/com/perfma/xlab/xpocket/plugin/jol/JOLXPocketCommand.java
@@ -13,14 +13,14 @@ import java.util.Arrays;
  * @description 用于实现每个命令的核心逻辑，一个或者多个命令指向一个类。
  * @date 7/31/2021 12:29
  */
-@CommandInfo(name = "help", usage = "How to use JOL", index = 0)
-@CommandInfo(name = "internals", usage = "Show the object internals: field layout and default contents, object header.", index = 1)
-@CommandInfo(name = "externals", usage = "Show the object externals: the objects reachable from a given instance.", index = 2)
-@CommandInfo(name = "estimates", usage = "Simulate the class layout in different VM modes.", index = 3)
-@CommandInfo(name = "footprint", usage = "Estimate the footprint of all objects reachable from a given instance.", index = 4)
-@CommandInfo(name = "heapdumpstats", usage = "Consume the heap dump and print the most frequent instances.", index = 5)
-@CommandInfo(name = "shapes", usage = "Dump the object shapes present in JAR files or heap dumps.", index = 6)
-@CommandInfo(name = "string-compress", usage = "Consume the heap dumps and figures out the savings attainable with compressed strings.", index = 7)
+@CommandInfo(name = "help", usage = "How to use JOL(如何使用JOL).", index = 0)
+@CommandInfo(name = "internals", usage = "Show the object internals: field layout and default contents, object header(显示对象内部:字段布局和默认内容，对象头).", index = 1)
+@CommandInfo(name = "externals", usage = "Show the object externals: the objects reachable from a given instance(显示对象外部:从实例可达的对象).", index = 2)
+@CommandInfo(name = "footprint", usage = "Estimate the footprint of all objects reachable from a given instance(估计从实例可达的对象的引用信息).", index = 3)
+@CommandInfo(name = "estimates", usage = "Simulate the class layout in different VM modes(模拟在不同VM模式下类的布局).", index = 4)
+@CommandInfo(name = "heapdumpstats", usage = "Consume the heap dump and print the most frequent instances(使用heap dump并打印最频繁的实例).", index = 5)
+@CommandInfo(name = "shapes", usage = "Dump the object shapes present in JAR files or heap dumps(Dump JAR文件或heap dumps中存在的对象结构).", index = 6)
+@CommandInfo(name = "string-compress", usage = "Consume the heap dumps and figures out the savings attainable with compressed strings(使用heap dumps并计算压缩字符串可获得的节省).", index = 7)
 public class JOLXPocketCommand extends AbstractXPocketCommand {
 
     private JOLXPocketPlugin jolxPocketPlugin;
@@ -34,10 +34,7 @@ public class JOLXPocketCommand extends AbstractXPocketCommand {
     public void invoke(XPocketProcess process) throws Throwable {
 
         jolxPocketPlugin.invoke(process);
-        /*XPocketProcessTemplate.execute(process,
-                (String cmd, String[] args) -> 
-                        String.format("EXECUTION %s %s",cmd , 
-                                args == null ? null : Arrays.toString(args)));*/
+
     }
 
 
